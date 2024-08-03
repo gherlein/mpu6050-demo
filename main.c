db8d8b0aecda74dad711d3c72708b2ecf1fb23b8
Greg Herlein <gherlein@herlein.com>
2024-08-03 05:33:35 -0700

    reading values

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include "pico/stdlib.h"
@@ -42,58 +43,146 @@ const uint LED_PIN = 25;
 //static int addr = 0x68;
 static int addr = 0x69;
 
-#ifdef i2c_default
-static void mpu6050_reset() {
-    // Two byte reset. First byte register, second byte data
+bool reserved_addr(uint8_t addr) {
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+
+static void scan(i2c_inst_t *i2c) {
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(i2c, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
+    
+}
+
+static void mpu6050_reset(i2c_inst_t *i2c) {
+   int rval;
+    printf("Resetting MPU6050\n");
+
+     // Two byte reset. First byte register, second byte data
     // There are a load more options to set up the device in different ways that could be added here
     uint8_t buf[] = {0x6B, 0x80};
-    i2c_write_blocking(i2c_default, addr, buf, 2, false);
-
+    rval=i2c_write_blocking(i2c, addr, buf, 2, false);
+    printf("rval: %d\n", rval);
     // from Hunter Adam's code:
     // Set gyro sample rate (set to 1KHz, same as accel)
-    uint8_t gyro_rate[] = {0x19, 0b00000111} ;
-    i2c_write_blocking(i2c_default, addr, gyro_rate, 2, false);
+    //uint8_t gyro_rate[] = {0x19, 0b00000111} ;
+    //i2c_write_blocking(i2c, addr, gyro_rate, 2, false);
 
     // Configure the Gyro range (+/- 250 deg/s)
-    uint8_t gyro_settings[] = {0x1b, 0b00000000} ;
-    i2c_write_blocking(i2c_default, addr, gyro_settings, 2, false);
+    //uint8_t gyro_settings[] = {0x1b, 0b00000000} ;
+    //i2c_write_blocking(i2c, addr, gyro_settings, 2, false);
 
     // Configure the Accel range (+/- 2g's)
-    uint8_t accel_settings[] = {0x1c, 0b00000000} ;
-    i2c_write_blocking(i2c_default, addr, accel_settings, 2, false);
+    //uint8_t accel_settings[] = {0x1c, 0b00000000} ;
+    //i2c_write_blocking(i2c, addr, accel_settings, 2, false);
 
 #if 0
     // Configure interrupt pin
     uint8_t pin_settings[] = {0x37, 0b00010000} ;
-    i2c_write_blocking(i2c_default, addr, pin_settings, 2, false);
+    i2c_write_blocking(i2c, addr, pin_settings, 2, false);
 
     // Configure data ready interrupt
     uint8_t int_config[] = {0x38, 0x01} ;
-    i2c_write_blocking(i2c_default, addr, int_config, 2, false);
+    i2c_write_blocking(i2c, addr, int_config, 2, false);
 #endif
 }
 
-static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+
+void mpu6050_read_raw(i2c_inst_t *i2c,int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     // For this particular device, we send the device the register we want to read
     // first, then subsequently read from the device. The register is auto incrementing
     // so we don't need to keep sending the register we want, just the first.
 
     uint8_t buffer[6];
+    int16_t temp_accel, temp_gyro ;
+    int rval;
+
+    printf("Reading MPU6050\n");
 
     // Start reading acceleration registers from register 0x3B for 6 bytes
     uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
-    i2c_read_blocking(i2c_default, addr, buffer, 6, false);
+    i2c_write_blocking(i2c, addr, &val, 1, true); // true to keep master control of bus
+    i2c_read_blocking(i2c, addr, buffer, 6, false);
 
     for (int i = 0; i < 3; i++) {
-        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+        temp_accel = (buffer[i<<1] << 8 | buffer[(i<<1) + 1]);
+        accel[i] = temp_accel ;
+        accel[i] <<= 2 ; // convert to g's (fixed point)
     }
 
     // Now gyro data from reg 0x43 for 6 bytes
     // The register is auto incrementing on each read
     val = 0x43;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true);
-    i2c_read_blocking(i2c_default, addr, buffer, 6, false);  // False - finished with bus
+    i2c_write_blocking(i2c, addr, &val, 1, true);
+    i2c_read_blocking(i2c, addr, buffer, 6, false);  // False - finished with bus
+
+    for (int i = 0; i < 3; i++) {
+        temp_gyro = (buffer[i<<1] << 8 | buffer[(i<<1) + 1]);
+        gyro[i] = temp_gyro ;
+        //gyro[i] = (gyro[i], 500<<16) ; // deg/sec
+    }
+    // Now temperature from reg 0x41 for 2 bytes
+    // The register is auto incrementing on each read
+    val = 0x41;
+    i2c_write_blocking(i2c, addr, &val, 1, true);
+    rval=i2c_read_blocking(i2c, addr, buffer, 2, false);  // False - finished with bus
+    printf("rval: %d\n", rval);
+    *temp = buffer[0] << 8 | buffer[1];
+
+    gpio_put(LED_PIN, 1);
+    sleep_ms(250);
+    gpio_put(LED_PIN, 0);
+}
+static void mpu6050_read_raw_sdk(i2c_inst_t *i2c,int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+    // For this particular device, we send the device the register we want to read
+    // first, then subsequently read from the device. The register is auto incrementing
+    // so we don't need to keep sending the register we want, just the first.
+
+    uint8_t buffer[6];
+    int rval;
+
+    // Start reading acceleration registers from register 0x3B for 6 bytes
+    uint8_t val = 0x3B;
+    i2c_write_blocking(i2c, addr, &val, 1, true); // true to keep master control of bus
+    rval=i2c_read_blocking(i2c, addr, buffer, 6, false);
+    printf("rval: %d\n", rval);
+
+    printf("%x %x %x %x %x %x\n", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5]);
+
+    for (int i = 0; i < 3; i++) {
+        accel[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);
+        printf("%d", accel[i]);
+    }
+    printf("\n");
+
+    // Now gyro data from reg 0x43 for 6 bytes
+    // The register is auto incrementing on each read
+    val = 0x43;
+    i2c_write_blocking(i2c, addr, &val, 1, true);
+    rval=i2c_read_blocking(i2c, addr, buffer, 6, false);  // False - finished with bus
+    printf("rval: %d\n", rval);
 
     for (int i = 0; i < 3; i++) {
         gyro[i] = (buffer[i * 2] << 8 | buffer[(i * 2) + 1]);;
@@ -102,9 +191,9 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     // Now temperature from reg 0x41 for 2 bytes
     // The register is auto incrementing on each read
     val = 0x41;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true);
-    i2c_read_blocking(i2c_default, addr, buffer, 2, false);  // False - finished with bus
-
+    i2c_write_blocking(i2c, addr, &val, 1, true);
+    rval=i2c_read_blocking(i2c, addr, buffer, 2, false);  // False - finished with bus
+    printf("rval: %d\n", rval);
     *temp = buffer[0] << 8 | buffer[1];
 
     gpio_put(LED_PIN, 1);
@@ -113,40 +202,44 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     //puts("Hello World\n");
     
 }
-#endif
+
 
 #define NORMAL
 #ifdef NORMAL
 int main() {
-    stdio_init_all();
+    i2c_inst_t *i2c = i2c0;
+    const uint sda_pin = 8 ;
+    const uint scl_pin = 9;
 
+    stdio_init_all();// Pins
+    
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-    #warning i2c/mpu6050_i2c example requires a board with I2C pins
-    puts("Default I2C pins were not defined");
-    return 0;
-#else
-    printf("Hello, MPU6050! Reading raw data from registers...\n");
+    sleep_ms(5000);
+    printf("Hello, MPU6050! Reading raw data from registers using SCL %d and SDA %d\n",sda_pin,scl_pin);
     sleep_ms(2000);
 
-    // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
-    i2c_init(i2c_default, 400 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+#define RUN_COLLECTION
+#ifdef RUN_COLLECTION
+    i2c_init(i2c, 400 * 1000);
+    gpio_set_function(sda_pin, GPIO_FUNC_I2C);
+    gpio_set_function(scl_pin, GPIO_FUNC_I2C);
+    gpio_pull_up(sda_pin);
+    gpio_pull_up(scl_pin);
+     //bi_decl(bi_2pins_with_func(I2C_AUDIO_SDA_PIN, I2C_AUDIO_SCL_PIN, GPIO_FUNC_I2C));
     
-    // Make the I2C pins available to picotool
-    //bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-
-    mpu6050_reset();
+        //mpu6050_reset(i2c);
+    
+//    while (1) {
+        scan(i2c);
+//        sleep_ms(5000);
+//    }
 
     int16_t acceleration[BUF_LEN], gyro[BUF_LEN], temp;
 
     while (1) {
-        mpu6050_read_raw(acceleration, gyro, &temp);
+        mpu6050_read_raw(i2c, acceleration, gyro, &temp);
 
         absolute_time_t timestamp = get_absolute_time();
         printf("Time: %lld\n", timestamp);
@@ -159,33 +252,35 @@ int main() {
         // Note this is chip temperature.
         printf("Temp. = %f\n", (temp / 340.0) + 36.53);
         printf("********************************************************************************************\n");
-        sleep_ms(1000);
+        
         memset(acceleration, 0, BUF_LEN);
         memset(gyro, 0, BUF_LEN);
         temp = 0;
-        
+        sleep_ms(1000);
     }
 #endif
 }
 #endif
 
+
+//#define BUS_SCAN
+#ifdef BUS_SCAN
 // I2C reserves some addresses for special purposes. We exclude these from the scan.
 // These are any addresses of the form 000 0xxx or 111 1xxx
 bool reserved_addr(uint8_t addr) {
     return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 }
 
-//#define BUS_SCAN
-#ifdef BUS_SCAN
+
 int main() {
     // Enable UART so we can print status output
     stdio_init_all();
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
+#if !defined(i2c) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
 #warning i2c/bus_scan example requires a board with I2C pins
     puts("Default I2C pins were not defined");
 #else
     // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
-    i2c_init(i2c_default, 100 * 1000);
+    i2c_init(i2c, 100 * 1000);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
@@ -212,7 +307,7 @@ int main() {
         if (reserved_addr(addr))
             ret = PICO_ERROR_GENERIC;
         else
-            ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+            ret = i2c_read_blocking(i2c, addr, &rxdata, 1, false);
 
         printf(ret < 0 ? "." : "@");
         printf(addr % 16 == 15 ? "\n" : "  ");
